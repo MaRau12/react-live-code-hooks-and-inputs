cc0114a61ab854be2a7357158a7ad0155dec432a
MaRau12 <marthaelisabeth94@gmail.com>
2023-02-28 18:51:28 +0000

    hooks and inputs

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import ControlledInput from "./controlledInput.jsx";
+import UncontrolledInput from "./UncontrolledInput.jsx";
 
 //create your first component
 const Home = () => {
+	const [on, setOn] = useState(true)
+	console.log("Render home Component")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	<>
+		   {on ? 
+		   (<>
+		   <button onClick={()=> setOn(false)}>Show</button> 
+		    <ControlledInput/>
+			</>
+			): (
+			<>
+			<button onClick={() => setOn(true)}>Hide</button>
+			<UncontrolledInput/>
+			</>
+			)}
+		  
+    </>
 	);
 };
 
